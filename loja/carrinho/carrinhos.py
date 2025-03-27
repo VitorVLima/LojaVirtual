d0cc72f7b1e3b48aae2f03fed18921dcf1a79169
VitorVLima <vitor14_df@hotmail.com>
2025-03-27 12:46:57 -0300

    att pra n descarregar

--- loja/carrinho/carrinhos.py
+++ loja/carrinho/carrinhos.py
@@ -16,11 +16,11 @@ def addCarrinho():
     try:
         produto_id = request.form.get('produto_id')
         quantity = request.form.get('quantity')
-        colors = request.form.get('colors')
+        color = request.form.get('colors')
         produto = Addproduto.query.filter_by(id = produto_id).first()
 
-        if produto_id and quantity and colors and request.method == 'POST':
-            DicItems = {produto_id:{'name':produto.name,'price':float(produto.price), 'discount': produto.discount, 'colors': produto.colors, 'quantity': quantity, 'image': produto.image_1}}
+        if produto_id and quantity and color and request.method == 'POST':
+            DicItems = {produto_id:{'name':produto.name,'price':float(produto.price), 'discount': produto.discount, 'color': color, 'quantity': quantity, 'image': produto.image_1, 'colors': produto.colors, 'stock': produto.stock}}
             if 'LojainCarrinho' in session:
                 print(session['LojainCarrinho'])
                 if produto_id in session['LojainCarrinho']:
@@ -39,8 +39,47 @@ def addCarrinho():
 
 @app.route('/carros')
 def getCart():
-    marcas = Marca.query.join(Addproduto,(Marca.id == Addproduto.marca_id)).all()
-    categorias = Categoria.query.join(Addproduto,(Categoria.id == Addproduto.categoria_id)).all()
+    marcas = Marca.query.join(Addproduto, (Marca.id == Addproduto.marca_id)).all()
+    categorias = Categoria.query.join(Addproduto, (Categoria.id == Addproduto.categoria_id)).all()
+
     if 'LojainCarrinho' not in session:
         return redirect(request.referrer)
-    return render_template('produtos/carros.html', marcas = marcas, categorias = categorias, title="Carrinho de Compras")
+
+    subtotal1 = 0
+    for key, produto in session['LojainCarrinho'].items():
+        discount = (produto['discount'] / 100) * float(produto['price'])
+        subtotal1 += (float(produto['price']) - discount) * int(produto['quantity'])
+
+    # Imposto é calculado após o subtotal, com 6% do valor total após os descontos
+    imposto = round(0.06 * subtotal1, 2)  # Imposto de 6%
+    valorpagar = round(subtotal1 + imposto, 2)
+
+    return render_template('produtos/carros.html', marcas = marcas, categorias = categorias, title="Carrinho de Compras", imposto = imposto, valorpagar = valorpagar)
+
+@app.route('/updateCarro/<int:code>', methods = ['POST'])
+def updateCarro(code):
+    if 'LojainCarrinho' not in session and len(session['LojainCarrinho']) < 0:
+        return redirect(url_for('home'))
+    if request.method == 'POST':
+        quantity = request.form.get('quantity')
+        color = request.form.get('color')
+        try:
+            session.modified = True
+            for key, item in session['LojainCarrinho'].items():
+                if int(key) == code:
+                    item['quantity'] = quantity
+                    item['color'] = color
+                    flash('Carrinho foi Atualizado')
+                    return redirect(url_for('getCart'))
+        except Exception as e:
+            print(e)
+            return redirect(url_for('getCart'))
+
+
+@app.route('/vazio')
+def vazio_Cart():
+    try:
+        session.clear()
+        return redirect(url_for('home'))
+    except Exception as e:
+        print(e)
